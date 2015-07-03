e8afea2b2128dfd172c1dafbc01164a396025154
don-philipe <don_philipe@gmx.de>
2015-07-03 09:38:02 +0200

    now proper junit test

--- app/src/test/java/tud/inf/smime4android/KeyStoreHandlerTest.java
+++ app/src/test/java/tud/inf/smime4android/KeyStoreHandlerTest.java
@@ -1,21 +1,22 @@
 package tud.inf.smime4android;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 import java.io.File;
 
+import static org.junit.Assert.*;
+
 /**
- * Created by don on 02.07.15.
+ * Created by don on 03.07.15.
  */
 public class KeyStoreHandlerTest {
 
     @Test
-    public void testInitKeyStore() {
+    public void testInitKeyStore() throws Exception {
         String filePath = "keystore.file";
         File ksFile = new File(filePath);
         char[] passwd = "1q2w3e4r".toCharArray();
         KeyStoreHandler.initKeyStore(ksFile, passwd);
-        assertNotEquals(0, ksFile.length());
+        assertEquals(0, ksFile.length());
     }
 }
