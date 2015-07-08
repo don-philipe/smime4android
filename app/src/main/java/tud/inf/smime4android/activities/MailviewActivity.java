7c85570e518d4d063a690b171c836d94cf900d88
roegnarak <roegnarak@gmail.com>
2015-07-08 11:11:32 +0200

    MessageView mit Base64

--- app/src/main/java/tud/inf/smime4android/activities/MailviewActivity.java
+++ app/src/main/java/tud/inf/smime4android/activities/MailviewActivity.java
@@ -1,14 +1,21 @@
 package tud.inf.smime4android.activities;
 
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.ByteArrayOutputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
 
 import tud.inf.smime4android.logic.DecryptMail;
 import tud.inf.smime4android.R;
@@ -39,6 +46,17 @@ public class MailviewActivity extends ActionBarActivity {
         // TODO read password from stdin ;)
         String password = "1q2w3e4r";
         if(intent.getData()!=null) {
+            //DecryptVerifyResult result = intent.getParcelableExtra(EXTRA_METADATA);
+
+            String plaintext;
+            try {
+                plaintext = readTextFromUri(this, intent.getData(), "base64");
+                DecryptMail dm = new DecryptMail(this);
+                content.setText(plaintext);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+/*
             try {
                 DecryptMail dm = new DecryptMail(this);
                 content.setText(dm.decrypt(ksPath, password.toCharArray(), this.getContentResolver().openInputStream(data))
@@ -48,6 +66,7 @@ public class MailviewActivity extends ActionBarActivity {
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
             }
+            */
         }
         //}
     }
@@ -72,5 +91,39 @@ public class MailviewActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public static String readTextFromUri(Context context, Uri outputUri, String charset)
+            throws IOException {
+
+        byte[] decryptedMessage;
+        {
+            InputStream in = context.getContentResolver().openInputStream(outputUri);
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            byte[] buf = new byte[256];
+            int read;
+            while ( (read = in.read(buf)) > 0) {
+                out.write(buf, 0, read);
+            }
+            in.close();
+            out.close();
+            decryptedMessage = out.toByteArray();
+        }
+
+        String plaintext;
+       /* if (charset != null) {
+            try {
+                plaintext = new String(decryptedMessage, charset);
+            } catch (UnsupportedEncodingException e) {
+                // if we can't decode properly, just fall back to utf-8
+                plaintext = new String(decryptedMessage);
+            }
+        } else {
+            plaintext = new String(decryptedMessage);
+        }*/
+        plaintext = Base64.encodeToString(decryptedMessage,Base64.DEFAULT);
+
+        return plaintext;
+
     }
 }
