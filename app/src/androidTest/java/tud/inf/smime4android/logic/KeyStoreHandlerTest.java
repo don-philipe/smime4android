69d09e70f8ad9c640164ee00307462db233d97ba
don-philipe <don_philipe@gmx.de>
2015-09-22 15:59:03 +0200

    added test for private key and certificate

--- app/src/androidTest/java/tud/inf/smime4android/logic/KeyStoreHandlerTest.java
+++ app/src/androidTest/java/tud/inf/smime4android/logic/KeyStoreHandlerTest.java
@@ -3,11 +3,23 @@ package tud.inf.smime4android.logic;
 import android.content.Context;
 import android.test.InstrumentationTestCase;
 
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.openssl.PEMKeyPair;
+import org.bouncycastle.openssl.PEMParser;
+import org.bouncycastle.openssl.jcajce.JcaPEMKeyConverter;
+
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.security.KeyStoreException;
+import java.security.NoSuchProviderException;
+import java.security.PrivateKey;
 import java.security.UnrecoverableKeyException;
+import java.security.cert.Certificate;
 import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.util.Enumeration;
 
 import tud.inf.smime4android.R;
 
@@ -58,6 +70,57 @@ public class KeyStoreHandlerTest extends InstrumentationTestCase {
             ksh.storeKeyStore();
 
             assertEquals(true, ksh.getAliases().hasMoreElements());
+
+        } catch (CertificateException e) {
+            e.printStackTrace();
+        } catch (KeyStoreException e) {
+            e.printStackTrace();
+        } catch (UnrecoverableKeyException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 
+     */
+    public void testGetPrivateKeyAndCert() {
+        Context targetcontext = getInstrumentation().getTargetContext();
+        targetcontext.deleteFile(targetcontext.getResources().getString(R.string.ks_filename));
+        char[] ks_passwd = {'p', 'a', 's', 's', 'w', 'd'};
+
+        try {
+            KeyStoreHandler ksh = new KeyStoreHandler(targetcontext, ks_passwd);
+            InputStream inp12 = targetcontext.getResources().openRawResource(R.raw.key_and_cert_p12);
+            char[] p12_passwd = {'p', 'a', 's', 's', 'w', 'o', 'r', 'd'};
+
+            ksh.importPKCS12(inp12, p12_passwd, null);
+            ksh.storeKeyStore();
+
+            InputStream keystream = targetcontext.getResources().openRawResource(R.raw.key_pem);
+            PrivateKey exp_key = new JcaPEMKeyConverter().setProvider("BC")
+                    .getKeyPair((PEMKeyPair) (new PEMParser(new InputStreamReader(keystream))).readObject()).getPrivate();
+            InputStream certstream = targetcontext.getResources().openRawResource(R.raw.cert_pem);
+            X509Certificate exp_cert = null;
+            try {
+                exp_cert = (X509Certificate) CertificateFactory.getInstance("X.509", BouncyCastleProvider.PROVIDER_NAME)
+                        .generateCertificate(certstream);
+            } catch (NoSuchProviderException e) {
+                e.printStackTrace();
+            }
+
+            Enumeration<String> aliases = ksh.getAliases();
+            PrivateKey key = null;
+            Certificate cert = null;
+            while(aliases.hasMoreElements()) {
+                String alias = aliases.nextElement();
+                key = ksh.getPrivateKey(alias, null);
+                cert = ksh.getCertificate(alias);
+            }
+
+            assertEquals(new String(exp_key.getEncoded()), new String(key.getEncoded()));
+            assertEquals(new String(exp_cert.getEncoded()), new String(cert.getEncoded()));
 
         } catch (CertificateException e) {
             e.printStackTrace();
